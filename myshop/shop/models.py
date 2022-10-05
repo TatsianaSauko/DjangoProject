8fc1e963a5836f7103f797e17dc7c2998233d634
TatsianaSauko <miljanzowa@mail.ru>
2022-10-05 13:28:51 +0300

    add handlers

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -19,6 +20,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
 
 
 class Product(models.Model):
@@ -59,3 +64,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
