2f5873603ca84fbbd393c696d9a446254a2a12c5
TatsianaSauko <miljanzowa@mail.ru>
2022-10-10 11:31:20 +0300

    add sort(price, name)

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -5,13 +5,28 @@ from .models import Category, Product
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    products = Product.objects.filter(available=True).order_by(
+        get_order_by_products(request))
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request, 'shop/product/list.html',
                   {'category': category, 'categories': categories,
                    'products': products})
+
+
+def get_order_by_products(request):
+    order_by = ''
+    if request.GET.__contains__('sort') and request.GET.__contains__('up'):
+        sort = request.GET['sort']
+        up = request.GET['up']
+        if sort == 'price' or sort == 'name':
+            if up == '0':
+                order_by = '-'
+            order_by += sort
+    if not order_by:
+        order_by = '-created'
+    return order_by
 
 
 def product_detail(request, id, slug):
